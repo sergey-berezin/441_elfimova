a8fb8193e7988821c5e00b2bc9288a5dd12d228b
QuantumDealer <62072915+QuantumDealer@users.noreply.github.com>
2022-12-08 01:31:12 +0300

    change db

--- grid/Grid/init.cs
+++ grid/Grid/init.cs
@@ -24,12 +24,28 @@ public class EmotionsTable
 {
     [Key]
     public int imageId { get; set; }
-    public Dictionary<string, float> emotions { get; set; }
+    //public Dictionary<string, float> emotions { get; set; }
+    public float neutral { get; set; }
+    public float happiness { get; set; }
+    public float surprise { get; set; }
+    public float sadness { get; set; }
+    public float anger { get; set; }
+    public float disgust { get; set; }
+    public float fear { get; set; }
+    public float contempt { get; set; }
     public EmotionsTable() { }
     public EmotionsTable(int imageId, Dictionary<string, float> emotions)
     {
         this.imageId = imageId;
-        this.emotions = emotions;
+        neutral = emotions["neutral"];
+        happiness = emotions["happiness"];
+        surprise = emotions["surprise"];
+        sadness = emotions["sadness"];
+        anger = emotions["anger"];
+        disgust = emotions["disgust"];
+        fear = emotions["fear"];
+        contempt = emotions["contempt"];
+        //this.emotions = emotions;
     }
 }
 
